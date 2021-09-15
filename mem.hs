dca79e70f96c2c73390f1b55e29e513ba6a1de01
fourofclubs <fourofclubs@gmail.com>
2021-09-14 21:52:50 -0500

    Delinting

--- mem.hs
+++ mem.hs
@@ -1,31 +1,41 @@
 module Mem
-(Deck,
-Value(..),
-Suit(..),
-Card(..),
-allValues,
-allSuits,
-(/\),
-cutAt,
-cutHalf,
-inF,
-outF,
-inFTimes,
-outFTimes,
-toCard,
-micholsonStack
-) where
+  ( Deck,
+    Value (..),
+    Suit (..),
+    Card (..),
+    allValues,
+    allSuits,
+    (/\),
+    cutAt,
+    cutHalf,
+    inF,
+    outF,
+    inFTimes,
+    outFTimes,
+    toCard,
+    micholsonStack,
+  )
+where
 
-import Data.List as List
-import Data.Maybe as Maybe
+import Data.List as List (elemIndex)
+import Data.Maybe as Maybe (fromJust)
 
 data Value = TWO | THREE | FOUR | FIVE | SIX | SEVEN | EIGHT | NINE | TEN | JACK | QUEEN | KING | ACE deriving (Show, Read, Eq, Ord, Enum, Bounded)
+
+allValues :: [Value]
 allValues = [minBound .. maxBound] :: [Value]
+
 data Suit = SPADES | HEARTS | CLUBS | DIAMONDS deriving (Show, Read, Eq, Enum, Bounded)
+
+allSuits :: [Suit]
 allSuits = [minBound .. maxBound] :: [Suit]
+
 data Card = Card {value :: Value, suit :: Suit} deriving (Eq)
+
 instance Show Card where
-    show (Card v s) = show v ++ " of " ++ show s
+  show (Card v s) = show v ++ " of " ++ show s
+
+(/\) :: Value -> Suit -> Card
 (/\) = Card
 
 type Deck = [Card]
@@ -33,23 +43,23 @@ type Deck = [Card]
 weave :: [a] -> [a] -> [a]
 weave [] as = as
 weave as [] = as
-weave as bs = (head as) : (weave bs (tail as))
+weave as bs = head as : weave bs (tail as)
 
 cutAt :: Int -> [a] -> [a]
-cutAt n as = (drop n as) ++ (take n as)
+cutAt n as = drop n as ++ take n as
 
-cutHalf :: (RealFrac r) => (r -> Int) -> [a] -> ([a],[a])
-cutHalf round as = splitAt (round . (/2) . fromIntegral . length $ as) as
+cutHalf :: (RealFrac r) => (r -> Int) -> [a] -> ([a], [a])
+cutHalf round as = splitAt (round . (/ 2) . fromIntegral . length $ as) as
 
 outF :: [a] -> [a]
-outF as = 
-    let (top, bottom) = cutHalf ceiling as
-    in weave top bottom
+outF as =
+  let (top, bottom) = cutHalf ceiling as
+   in weave top bottom
 
 inF :: [a] -> [a]
-inF as = 
-    let (top, bottom) = cutHalf floor as 
-    in weave bottom top
+inF as =
+  let (top, bottom) = cutHalf floor as
+   in weave bottom top
 
 times :: Int -> (a -> a) -> (a -> a)
 times n f = foldl (.) id (replicate n f)
@@ -61,22 +71,72 @@ inFTimes :: Int -> [a] -> [a]
 inFTimes n = times n inF
 
 cardNum :: Deck -> Card -> Int
-cardNum d = fromJust . fmap (+1) . flip elemIndex d
+cardNum d = (+ 1) . fromJust . flip elemIndex d
 
 toCard :: Deck -> Int -> Card
-toCard d n = d !! (n-1)
+toCard d n = d !! (n -1)
 
-toCard' d n = toCard d ((n-1) `mod` (length d) + 1)
+toCard' :: Deck -> Int -> Card
+toCard' d n = toCard d ((n -1) `mod` length d + 1)
 
 micholsonStack :: Deck
-micholsonStack = 
-    [KING /\ SPADES, FOUR /\ HEARTS, KING /\ HEARTS, EIGHT /\ CLUBS, NINE /\ DIAMONDS, SIX /\ SPADES, EIGHT /\ DIAMONDS, THREE /\ SPADES, 
-    TEN /\ HEARTS, FIVE /\ CLUBS, KING /\ CLUBS, QUEEN /\ SPADES, FIVE /\ HEARTS, NINE /\ SPADES, SEVEN /\ HEARTS, TWO /\ CLUBS, 
-    TEN /\ CLUBS, FIVE /\ DIAMONDS, TWO /\ SPADES, FOUR /\ DIAMONDS, TWO /\ HEARTS, QUEEN /\ HEARTS, SEVEN /\ CLUBS, JACK /\ DIAMONDS,
-    EIGHT /\ SPADES, TEN /\ DIAMONDS, FIVE /\ SPADES, NINE /\ HEARTS, FOUR /\ CLUBS, QUEEN /\ CLUBS, ACE /\ DIAMONDS, THREE /\ HEARTS,
-    JACK /\ SPADES, SIX /\ HEARTS, ACE /\ CLUBS, NINE /\ CLUBS, SEVEN /\ DIAMONDS, FOUR /\ SPADES, SIX /\ DIAMONDS, ACE /\ SPADES,
-    JACK /\ HEARTS, SIX /\ CLUBS, KING /\ DIAMONDS, TEN /\ SPADES, QUEEN /\ DIAMONDS, SEVEN /\ SPADES, EIGHT /\ HEARTS, THREE /\ CLUBS,
-    JACK /\ CLUBS, THREE /\ DIAMONDS, ACE /\ HEARTS, TWO /\ DIAMONDS]
+micholsonStack =
+  [ KING /\ SPADES,
+    FOUR /\ HEARTS,
+    KING /\ HEARTS,
+    EIGHT /\ CLUBS,
+    NINE /\ DIAMONDS,
+    SIX /\ SPADES,
+    EIGHT /\ DIAMONDS,
+    THREE /\ SPADES,
+    TEN /\ HEARTS,
+    FIVE /\ CLUBS,
+    KING /\ CLUBS,
+    QUEEN /\ SPADES,
+    FIVE /\ HEARTS,
+    NINE /\ SPADES,
+    SEVEN /\ HEARTS,
+    TWO /\ CLUBS,
+    TEN /\ CLUBS,
+    FIVE /\ DIAMONDS,
+    TWO /\ SPADES,
+    FOUR /\ DIAMONDS,
+    TWO /\ HEARTS,
+    QUEEN /\ HEARTS,
+    SEVEN /\ CLUBS,
+    JACK /\ DIAMONDS,
+    EIGHT /\ SPADES,
+    TEN /\ DIAMONDS,
+    FIVE /\ SPADES,
+    NINE /\ HEARTS,
+    FOUR /\ CLUBS,
+    QUEEN /\ CLUBS,
+    ACE /\ DIAMONDS,
+    THREE /\ HEARTS,
+    JACK /\ SPADES,
+    SIX /\ HEARTS,
+    ACE /\ CLUBS,
+    NINE /\ CLUBS,
+    SEVEN /\ DIAMONDS,
+    FOUR /\ SPADES,
+    SIX /\ DIAMONDS,
+    ACE /\ SPADES,
+    JACK /\ HEARTS,
+    SIX /\ CLUBS,
+    KING /\ DIAMONDS,
+    TEN /\ SPADES,
+    QUEEN /\ DIAMONDS,
+    SEVEN /\ SPADES,
+    EIGHT /\ HEARTS,
+    THREE /\ CLUBS,
+    JACK /\ CLUBS,
+    THREE /\ DIAMONDS,
+    ACE /\ HEARTS,
+    TWO /\ DIAMONDS
+  ]
 
+micholsonNum :: Card -> Int
 micholsonNum = cardNum micholsonStack
+
+micholsonCard :: Int -> Card
 micholsonCard = toCard micholsonStack
--- pokerformulas.hs
+++ pokerformulas.hs
@@ -1,119 +1,137 @@
-import Mem
-import PokerHands
+import Control.Applicative
+import Control.Monad
 import Data.List as L
 import Data.Map as M
-import Control.Monad
 import Data.Maybe
 import Data.Ord
-import Control.Applicative
+import Mem
+import PokerHands
 
-main = do 
-    hand <- buildHand initialBuilder
-    let pd = playerDeals micholsonStack hand
-    if M.null pd then putStrLn "No deal found."
+main = do
+  hand <- buildHand initialBuilder
+  let pd = playerDeals micholsonStack hand
+  if M.null pd
+    then putStrLn "No deal found."
     else do
-        putStrLn "Players?"
-        mapM_ print (keys pd)
-        players <- readLn :: IO Int
-        putStrLn (show hand ++ ", " ++ show players ++ " players:")
-        print (pd ! players)
+      putStrLn "Players?"
+      mapM_ print (keys pd)
+      players <- readLn :: IO Int
+      putStrLn (show hand ++ ", " ++ show players ++ " players:")
+      print (pd ! players)
 
-data PredicateType = IsValue Value | IsSuit Suit | IsNotValue Value | ValueAtMost Value | IsCard Value Suit | IsAnything 
-    | And PredicateType PredicateType | Not PredicateType deriving (Show, Eq)
+data PredicateType
+  = IsValue Value
+  | IsSuit Suit
+  | IsNotValue Value
+  | ValueAtMost Value
+  | IsCard Value Suit
+  | IsAnything
+  | And PredicateType PredicateType
+  | Not PredicateType
+  deriving (Show, Eq)
 
 toF :: PredicateType -> Card -> Bool
-toF (IsValue v) = (==v) . value
-toF (IsSuit s) = (==s) . suit
-toF (IsNotValue v) = not . (toF $ IsValue v)
-toF (ValueAtMost v) = (<=v) . value
-toF (IsCard v s) = (==(v /\ s))
-toF (And p1 p2) = \c -> (toF p1 c) && (toF p2 c)
-toF (Not p') = not . (toF p')
+toF (IsValue v) = (== v) . value
+toF (IsSuit s) = (== s) . suit
+toF (IsNotValue v) = not . toF (IsValue v)
+toF (ValueAtMost v) = (<= v) . value
+toF (IsCard v s) = (== (v /\ s))
+toF (And p1 p2) = \c -> toF p1 c && toF p2 c
+toF (Not p') = not . toF p'
 toF IsAnything = const True
 
 straightValues :: Value -> [Value]
-straightValues v = L.take 5 [v, pred v ..] ++ (if v <= FIVE then [ACE] else [])
+straightValues v = L.take 5 [v, pred v ..] ++ ([ACE | v <= FIVE])
 
 cardsFor :: Hand -> [PredicateType]
 cardsFor (Pair v) = [IsValue v, IsValue v, IsNotValue v, IsNotValue v, IsNotValue v]
-cardsFor (TwoPair v1 v2) = [IsValue v1, IsValue v1, IsValue v2, IsValue v2, (IsNotValue v1) `And` (IsNotValue v2)]
+cardsFor (TwoPair v1 v2) = [IsValue v1, IsValue v1, IsValue v2, IsValue v2, IsNotValue v1 `And` IsNotValue v2]
 cardsFor (Trips v) = [IsValue v, IsValue v, IsValue v, IsNotValue v, IsNotValue v]
 cardsFor (Straight v) = L.map IsValue (straightValues v)
-cardsFor (Flush s v) = (IsCard v s) : (replicate 4 (And (IsSuit s) (ValueAtMost v)))
+cardsFor (Flush s v) = IsCard v s : replicate 4 (And (IsSuit s) (ValueAtMost v))
 cardsFor (FullHouse v1 v2) = [IsValue v1, IsValue v1, IsValue v1, IsValue v2, IsValue v2]
 cardsFor (Quads v) = [IsValue v, IsValue v, IsValue v, IsValue v, IsAnything]
-cardsFor (StraightFlush v s) = L.map (flip IsCard s) (straightValues v)
+cardsFor (StraightFlush v s) = L.map (`IsCard` s) (straightValues v)
 
 type Deal = (Int, [Int])
+
 type Players = Int
 
 findLastIndex :: (a -> Bool) -> [a] -> Maybe Int
-findLastIndex p as = case (findIndices p as) of
-    [] -> Nothing
-    is -> Just (last is)
+findLastIndex p as = case findIndices p as of
+  [] -> Nothing
+  is -> Just (last is)
 
 searchFromTop :: Deck -> Players -> [Card -> Bool] -> Maybe [Int]
 searchFromTop d p [] = Just []
-searchFromTop d p (cp:cps) = (dealOne p cp d) >>= (\(n, d') -> fmap (n:) (searchFromTop d' p cps))
+searchFromTop d p (cp : cps) = dealOne p cp d >>= (\(n, d') -> fmap (n :) (searchFromTop d' p cps))
 
 dealOne :: Players -> (Card -> Bool) -> Deck -> Maybe (Int, Deck)
 dealOne p cp d
-    | length d < p = Nothing
-    | otherwise = deal p cp d <|> deal' p cp d <|> deal'' p cp d
+  | length d < p = Nothing
+  | otherwise = deal p cp d <|> deal' p cp d <|> deal'' p cp d
 
 --second deal to players
 deal :: Players -> (Card -> Bool) -> Deck -> Maybe (Int, Deck)
-deal p cp d = let (top, bottom) = L.splitAt p d in
-    fmap (\a -> (a,bottom)) . fmap ((`mod` p) . (+1)) $ (findLastIndex cp top)
+deal p cp d =
+  let (top, bottom) = L.splitAt p d
+   in fmap (\a -> (a, bottom) . ((`mod` p) . (+ 1))) (findLastIndex cp top)
 
 --second deal to dealer
 deal' :: Players -> (Card -> Bool) -> Deck -> Maybe (Int, Deck)
-deal' p cp d = let (top, bottom) = L.splitAt p d in if L.null bottom then Nothing else 
-    if cp (head bottom) then Just (p, (last top : tail bottom)) else Nothing
+deal' p cp d =
+  let (top, bottom) = L.splitAt p d
+   in if L.null bottom
+        then Nothing
+        else if cp (head bottom) then Just (p, last top : tail bottom) else Nothing
 
 --bottom deal
 deal'' :: Players -> (Card -> Bool) -> Deck -> Maybe (Int, Deck)
-deal'' p cp d = let (top, bottom) = L.splitAt p d in if L.null bottom then Nothing else
-    if cp (last bottom) then Just ((-1), (last top : init bottom)) else Nothing
+deal'' p cp d =
+  let (top, bottom) = L.splitAt p d
+   in if L.null bottom
+        then Nothing
+        else if cp (last bottom) then Just (-1, last top : init bottom) else Nothing
 
 searchFrom :: Int -> Deck -> Players -> [Card -> Bool] -> Maybe [Int]
-searchFrom n = searchFromTop . (cutAt n)
+searchFrom n = searchFromTop . cutAt n
 
 evaluateDeal :: Players -> Deal -> Int
-evaluateDeal p d = sum . L.map (\x -> if x == 0 then (p+1) else x) $ (snd d)
+evaluateDeal p d = sum . L.map (\x -> if x == 0 then p + 1 else x) $ snd d
 
 bestDeal :: Players -> [Deal] -> Maybe Deal
 bestDeal p [] = Nothing
-bestDeal p ds = Just . (maximumBy . comparing $ (evaluateDeal p)) $ ds
+bestDeal p ds = Just . (maximumBy . comparing $ evaluateDeal p) $ ds
 
 search' :: Deck -> Players -> [Card -> Bool] -> Maybe Deal
-search' d p cps = 
-    let deals = catMaybes [sequence (i, searchFrom i d p cps) | i <- [1 .. length d]]
-    in bestDeal p deals
+search' d p cps =
+  let deals = catMaybes [sequence (i, searchFrom i d p cps) | i <- [1 .. length d]]
+   in bestDeal p deals
 
 search :: Deck -> Players -> Hand -> Maybe Deal
-search d p h = 
-    let cards = (L.map . L.map $ toF) $ (nub . permutations . cardsFor $ h)
-        deals = catMaybes . L.map (search' d p) $ cards
-    in bestDeal p deals
+search d p h =
+  let cards = (L.map . L.map $ toF) (nub . permutations . cardsFor $ h)
+      deals = mapMaybe (search' d p) cards
+   in bestDeal p deals
 
 playerDeals :: Deck -> Hand -> Map Players Deal
 playerDeals d h = M.fromList . catMaybes $ [sequence (p, search d p h) | p <- [3 .. 10]]
 
 selectDeal :: Deck -> [Int] -> HandBuilder -> (Hand, Maybe Deal)
-selectDeal d (s1:s2:ss) hb = either (selectDeal d (s2:ss)) (\h -> (h,(search d s2 h))) (selectOption hb s1)
+selectDeal d (s1 : s2 : ss) hb = either (selectDeal d (s2 : ss)) (\h -> (h, search d s2 h)) (selectOption hb s1)
 selectDeal _ _ _ = error "Not enough selections."
 
 data Menu v = Menu String [Menu v] | MenuItem String v deriving (Show, Eq)
+
 instance Functor Menu where
-    fmap f (MenuItem s v) = MenuItem s (f v)
-    fmap f (Menu s ms) = Menu s (L.map (fmap f) ms)
+  fmap f (MenuItem s v) = MenuItem s (f v)
+  fmap f (Menu s ms) = Menu s (L.map (fmap f) ms)
 
 menuItem :: Show v => v -> Menu v
 menuItem v = MenuItem (show v) v
 
 menuItems :: Show k => [(k, v)] -> [Menu v]
-menuItems = L.map (\(k,v) -> MenuItem (show k) v)
+menuItems = L.map (\(k, v) -> MenuItem (show k) v)
 
 initialMenu :: Menu (Either HandBuilder Hand)
 initialMenu = MenuItem (prompt initialBuilder) (Left initialBuilder)
@@ -123,10 +141,10 @@ expand f (MenuItem s v) = Menu s (f v)
 expand f (Menu s ms) = Menu s (L.map (expand f) ms)
 
 expandAll :: Menu (Either HandBuilder Hand) -> Menu Hand
-expandAll = expand (either (fmap expandAll . selectMenuItems) ((:[]) . menuItem))
+expandAll = expand (either (fmap expandAll . selectMenuItems) ((: []) . menuItem))
 
 selectAll :: HandBuilder -> [(String, Either HandBuilder Hand)]
-selectAll hb = L.map (fmap (selectOption hb)) (flip zip [1..] (optionList hb))
+selectAll hb = zipWith (curry (fmap (selectOption hb))) (optionList hb) [1 ..]
 
 selectMenuItems :: HandBuilder -> [Menu (Either HandBuilder Hand)]
 selectMenuItems = menuItems . selectAll
@@ -136,4 +154,4 @@ formatMenu = unlines . formatMenu'
 
 formatMenu' :: Menu a -> [String]
 formatMenu' (MenuItem s v) = [s]
-formatMenu' (Menu s ts) = s : (fmap ('\t':) (ts >>= formatMenu'))
+formatMenu' (Menu s ts) = s : fmap ('\t' :) (ts >>= formatMenu')
